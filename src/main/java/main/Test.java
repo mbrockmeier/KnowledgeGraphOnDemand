e3e9568720bedffaca36ba00d38218dbbcf85315
Sunita Pateer <Sunita Pateer>
2020-11-07 20:54:47 +0100

    Added WikiBacklinksExtractor to extract back links of Wikipedia pages

--- src/main/java/main/Test.java
+++ src/main/java/main/Test.java
@@ -6,12 +6,22 @@ import extraction.StringToXml;
 import extraction.WikiPageExtractor;
 import org.w3c.dom.Document;
 
+import extraction.WikiBacklinksExtractor;
+
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("Fetch test...");
 
-        KnowledgeGraphBuilder.retrieveAndStoreWikipageXmlSource("Ethan Hunt");
+        String queryWikiPage = "Ethan Hunt";
+
+        KnowledgeGraphBuilder.retrieveAndStoreWikipageXmlSource(queryWikiPage);
         KnowledgeGraphBuilder.runExtractionFramework();
         KnowledgeGraphBuilder.decompressExtractedData();
+
+        WikiBacklinksExtractor extractor = new WikiBacklinksExtractor();
+        List<String> backlinks = extractor.getBackLinks(queryWikiPage);
+        System.out.println("Backlinks: " + backlinks);
     }
 }
