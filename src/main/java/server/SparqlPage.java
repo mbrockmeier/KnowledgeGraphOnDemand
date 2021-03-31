82f41b17535094bcf3796de172bb9233437a5a87
yawen <liuywmm@gmail.com>
2021-03-31 19:37:44 +0200

    update for sparql web view

--- src/main/java/server/SparqlPage.java
+++ src/main/java/server/SparqlPage.java
@@ -1,11 +1,17 @@
 package server;
 
 import extraction.KnowledgeGraphBuilder;
+import org.apache.jena.base.Sys;
 import org.apache.jena.rdf.model.Model;
+import parser.ModelCacheEntry;
 import sparql.RDFConnection_sparql;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+
+/**
+ * @author Yawen Liu
+ */
 
 @Path("/sparql/{resource}")
 public class SparqlPage {
@@ -23,5 +29,18 @@ public class SparqlPage {
         RDFConnection_sparql rdfConnection_spaqrql = new RDFConnection_sparql(query,model);
         String erg = rdfConnection_spaqrql.connect();
         return SparqlHTML.sparqlRender(erg);
+    }
+
+
+    @POST
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getSparqlJSON(@PathParam("resource") String resource, @QueryParam("wikiBaseUrl") String wikiBaseUrl,@FormParam("textarea") String sparql, @QueryParam("refreshModel") boolean refreshModel) {
+        System.out.println("call the sparql..........");
+        //ModelCacheEntry modelCacheEntry = KnowledgeGraphBuilder.getInstance().createKnowledgeGraphForWikiPage(wikiBaseUrl, resource, false, false);
+        JSONSparql jsonSparql = new JSONSparql();
+        System.out.println("sparql input: "+sparql);
+        //jsonSparql.init(modelCacheEntry,sparql);
+        //return jsonSparql.getJSON();
+        return "";
     }
 }
